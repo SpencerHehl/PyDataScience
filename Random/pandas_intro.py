a7e19b4208473e09b876edd925f4b87e0c83bd5b
SpencerHehl <schehl49@gmail.com>
2020-06-07 13:52:33 -0400

    make it cleaner

--- Random/pandas_intro.py
+++ Random/pandas_intro.py
@@ -2,6 +2,7 @@
 from google.cloud import bigquery
 from collections import defaultdict
 import pandas_gbq as pgbq
+import pandas as pd
 
 client = bigquery.Client()
 
@@ -10,15 +11,27 @@ query = """
     FROM `archerfrs-test.archer_device_telem.drone_battery`
 """
 data_frame = pgbq.read_gbq(query, project_id="archerfrs-test")  # Make an API request.
-
+data_frame_indexed = data_frame.set_index('timeSent')
 print("The query data:")
-print(data_frame)
+print(data_frame_indexed)
 
-print("The top 5 rows")
-print(data_frame.head(5))
+print('\n Describe the data')
+print(data_frame_indexed.describe())
 
-print("The bottom 5 rows")
-print(data_frame.tail(5))
+print('\n The top 5 rows')
+print(data_frame_indexed.head(5))
 
-print("Describe the data")
-print(data_frame.describe())
+print("\n The bottom 5 rows")
+print(data_frame_indexed.tail(5))
+
+print("\n Device Statistics")
+grouped_frame = data_frame_indexed.groupby(["deviceId",pd.Grouper(freq='1M')])["percentage"]
+
+print("Mean Battery")
+print(grouped_frame.mean())
+
+print("\n Max Battery")
+print(grouped_frame.max())
+
+print("\n Min Battery")
+print(grouped_frame.min())
